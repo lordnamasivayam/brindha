0df4cd54067502dd994073d59fb0fcfcd6eb1448
lordnamasivayam <50618802+lordnamasivayam@users.noreply.github.com>
2019-08-12 18:25:40 +0530

    Create pascal triangle.c

---
+++ pascal triangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n,r,ncr,s,row;
+scanf("%d",&row);
+for(n=0;n<row;n++)
+{
+for(s=n;s<row-1;s++)
+printf(" ");
+for(r=0;r<=n;r++)
+{
+if(n==0 || r==0)
+{
+ncr=1;
+printf("%d ",ncr);
+}
+else
+{
+ncr=ncr*(n-r+1)/r;
+printf("%d ",ncr);
+}
+}
+printf("\n");
+}
+return 0;
+}
